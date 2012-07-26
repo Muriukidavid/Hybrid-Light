ffc47f94489f2d9ce6c88d0204fbd5255fdb99bf
Muriukidavid <karfes@gmail.com>
2012-07-26 14:28:22 +0300

    the first commit

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *	Footer Template
+ *
+ *	necessary only because i wanted to position the copyright + powered by ( footer ) 
+ * after the body container
+ *
+ * @package Hybrid Light
+ * @subpackage Template
+*/
+?>
+
+		<?php do_atomic( 'after_container' ); // hybrid_after_container ?>
+
+	</div><!-- #container -->
+
+
+</div><!-- #body-container -->
+<div id="body-bottom"></div>
+<div id="footer-container">
+
+		<?php do_atomic( 'before_footer' ); // hybrid_before_footer ?>
+
+		<div id="footer">
+
+			<?php do_atomic( 'footer' ); // hybrid_footer ?>
+
+		</div><!-- #footer -->
+
+		<?php do_atomic( 'after_footer' ); // hybrid_after_footer ?>
+
+	</div><!-- #footer-container -->
+
+<?php do_atomic( 'after_html' ); // hybrid_after_html ?>
+
+<?php wp_footer(); // wp_footer ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ *	Header Template
+ *
+ *	necessary only because i wanted to position the site-title and description ( header )
+ *	outside the body container
+ *
+ * @package Hybrid Light
+ * @subpackage Template
+*/
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+<title><?php hybrid_document_title(); ?></title>
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<?php do_atomic( 'head' ); // @deprecated 0.9.0. Use 'wp_head'. ?>
+<?php wp_head(); // wp_head ?>
+
+</head>
+
+<body class="<?php hybrid_body_class(); ?>">
+
+<?php do_atomic( 'before_html' ); // hybrid_before_html ?>
+
+<?php do_atomic( 'before_header' ); // hybrid_before_header ?>
+
+	<div id="header-container">
+
+		<div id="header">
+
+			<?php do_atomic( 'header' ); // hybrid_header ?>
+
+		</div><!-- #header -->
+
+	</div><!-- #header-container -->
+
+	<?php do_atomic( 'after_header' ); // hybrid_after_header ?>
+	
+
+<div id="body-top"></div>
+<div id="body-container">
+	
+	<div id="container">
+
+		<?php do_atomic( 'before_container' ); // hybrid_before_container ?>
+		
